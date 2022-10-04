7df0aae65f00b87bf9d3a607a2d36207ad25c128
musialny.dev <musialny.dev@gmail.com>
2022-10-04 09:50:14 +0200

    Create unit tests

--- test/sort_test.exs
+++ test/sort_test.exs
@@ -1,8 +1,11 @@
 defmodule SortTest do
   use ExUnit.Case
-  doctest Sort
 
-  test "greets the world" do
-    assert Sort.hello() == :world
+  test "Sort.bsort/1 => Simple reversed list" do
+    assert Sort.bsort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  end
+
+  test "Sort.bsort/1 => \"Random\" list content" do
+    assert Sort.bsort([10, 1, 9, 2, 8, 3, 7, 4, 6, 5]) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   end
 end
